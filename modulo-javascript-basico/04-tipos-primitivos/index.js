c1968bc41e5b1e1e2219505822dad5adb6549304
Everson OLiveira <82684218+Eor13@users.noreply.github.com>
2022-09-22 18:51:03 -0300

    Nova Aula

---
+++ modulo-javascript-basico/04-tipos-primitivos/index.js
@@ -0,0 +1,7 @@
+let nome ='Everson'; //STRING
+let numero =10.6; //NUMBER
+let numero1 =-10.6; //NUMBER
+let numero2 =10.6666; //NUMBER
+let usuarioVerificado = true; //BOOLEAN
+
+let pessoa = null; //NULL
