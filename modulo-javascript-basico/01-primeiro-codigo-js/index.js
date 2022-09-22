6af9b28b3dd497ecce93c1ae7084b42a2e7f377e
Everson OLiveira <82684218+Eor13@users.noreply.github.com>
2022-09-22 18:23:56 -0300

    Novo Exercício JS

--- modulo-javascript-basico/01-primeiro-codigo-js/index.js
+++ modulo-javascript-basico/01-primeiro-codigo-js/index.js
@@ -1,3 +1,11 @@
-window.alert("Olá Everson!")
+// window.alert("Olá Everson!")
 // window.confirm("Deseja Continuar?")
 // window.prompt("Digite um Numero.")
+
+let retornoDoUsuario = confirm("Deseja continuar?")
+
+console.log(retornoDoUsuario)
+
+let idade = prompt("Digite sua idade:")
+
+console.log(idade)
---
+++ modulo-javascript-basico/03-variaveis/index.js
@@ -0,0 +1,10 @@
+{
+var nomePessoa = "Everson";
+const emailPessoal = "amaoido@gmail.com";
+let telefonePessoal = "15959-5296";
+}
+
+
+console.log(nomePessoa)
+console.log(telefonePessoal)
+console.log(emailPessoal)
