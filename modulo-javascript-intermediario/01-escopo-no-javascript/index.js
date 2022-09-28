0cd40a96436b59eb4eb5548de6835d3dc80f1270
Everson OLiveira <82684218+Eor13@users.noreply.github.com>
2022-09-27 22:41:48 -0300

    Novas Aulas

---
+++ modulo-javascript-intermediario/01-escopo-no-javascript/index.js
@@ -0,0 +1,47 @@
+
+/*
+if(true){
+    const mensagem ="Olá"
+    console.log(mensagem)
+}
+*/
+
+/*
+{
+    const mensagem ="Olá"
+    console.log(mensagem)
+}
+console.log(mensagem)
+*/
+
+/*
+function falar(){
+    var mensagem = "Olá Mundo!"
+    console.log(mensagem) //funciona
+}
+
+falar()
+console.log(mensagem) //não funciona
+*/
+
+/*
+function falar(){
+    var mensagem = "Olá Mundo!"
+    console.log(mensagem) //funciona
+    function falarOutraCoisa(){
+
+    }
+    falarOutraCoisa()
+    console.log(falarOutraCoisa)
+}
+
+falar()
+console.log(mensagem) //não funciona
+console.log(falarOutraCoisa)
+*/
+
+console.log(url)
+
+const numero = 123;
+
+console.log(numero)
---
+++ modulo-javascript-intermediario/02-hosting/index.js
@@ -0,0 +1,13 @@
+let fruta
+console.log(fruta)
+fruta = "Laranja"
+
+
+
+
+
+
+var pais; //declaração
+pais = "Brasi"; //atribuição ou inicialização
+
+var pais = "Brasil";
